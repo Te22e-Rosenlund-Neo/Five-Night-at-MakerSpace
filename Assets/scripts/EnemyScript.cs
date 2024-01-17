0ac15be3598bba1e610daaac1f9f90362bedda94
Virtknight <Neo.Rosenlund1@outlook.com>
2024-01-17 17:45:31 +0100

    optimised

--- Assets/scripts/EnemyScript.cs
+++ Assets/scripts/EnemyScript.cs
@@ -10,24 +10,26 @@ public class EnemyScript : MonoBehaviour
 {
     public List<GameObject> points = new();
     public int AI_Level;
-    public int timer;
+    private int timer = 0;
+    private int Ai_max = 20; 
+    public bool CanChangePosition = true;
+
     void Update(){
-
-if(timer >= 120){
-    if(ShouldSpawn(AI_Level) == true){
-        Debug.Log("I moved");
-        transform.position = RandomPosition(points).transform.position;
-    }
-
-
-    timer -= timer;
+if(CanChangePosition == true){
+    if(timer >= 360){
+        if(ShouldSpawn(AI_Level, Ai_max) == true){
+            Debug.Log("I moved");
+            transform.position = RandomPosition(points).transform.position;
+        }
+        timer -= timer;
 }
 
 
 timer++;
+}
     }
     GameObject RandomPosition(List<GameObject> points){
-        
+        //picks a random point in its list of walkable points
         int random = Random.Range(0,8);
         if(random == 0){
             
@@ -38,9 +40,9 @@ timer++;
         }
     }
 
-    bool ShouldSpawn(int AI_level){
-
-        int random3 = Random.Range(0, 20);
+    bool ShouldSpawn(int AI_level, int AI_max){
+        //compares if the enemy should walk depending on what difficulty it is on
+        int random3 = Random.Range(0, AI_max);
         if(random3 < AI_level){
             return true;
         }
