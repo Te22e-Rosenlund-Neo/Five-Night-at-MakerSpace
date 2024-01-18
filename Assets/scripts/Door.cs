bb5f9a80f127e9d6786abbbf4ea6ce6302234105
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-01-18 14:42:36 +0100

    send to pc

--- Assets/scripts/Door.cs
+++ Assets/scripts/Door.cs
@@ -6,28 +6,47 @@ using UnityEngine.UIElements;
 public class Door : MonoBehaviour
 {
     bool DoorOpen = false;
+    [SerializeField] float timeToOpen;
     [Header("2 Door Positions")]
     [SerializeField] Vector3 Top;
     [SerializeField] Vector3 Bottom;
     Vector3 Target;
-void Awake(){
-    Target = Bottom;
-}
-void Update(){
-    if(transform.position != Target){
-        transform.position = Vector3.MoveTowards(transform.position, Target, 0.02f);
-    }
-}
-
-public void ToggleDoor(){
-    if(DoorOpen == false){
-        Target = Top;
-        DoorOpen = true;
-    }else{
+    void Awake()
+    {
         Target = Bottom;
-        DoorOpen = false;
     }
-}
+    void FixedUpdate()
+    {
+        // if(transform.position != Target){
+        //     transform.position = Vector3.MoveTowards(transform.position, Target, 0.02f);
+        // }
+    }
+
+    public void ToggleDoor()
+    {
+        if (DoorOpen == false)
+        {
+            Target = Top;
+            DoorOpen = true;
+            ManageDoor();
+        }
+        else
+        {
+            Target = Bottom;
+            DoorOpen = false;
+        }
+    }
+
+    void ManageDoor()
+    {
+        float t = 0;
+        while (t < timeToOpen)
+        {
+            transform.position = Vector3.Lerp(Bottom, Top, t);
+
+            t += Time.deltaTime;
+        }
+    }
 
 }
 
