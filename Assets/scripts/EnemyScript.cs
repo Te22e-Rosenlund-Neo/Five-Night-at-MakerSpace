026b19ca58d437a53adfdde3ffdc5936ce14b1a0
Neo Rosenlund <Neo.Rosenlund@elev.ga.ntig.se>
2024-02-13 16:24:41 +0100

    Updated enemyscript to have improved code and mechanics

--- Assets/scripts/EnemyScript.cs
+++ Assets/scripts/EnemyScript.cs
@@ -11,53 +11,73 @@ public class EnemyScript : MonoBehaviour
     public List<GameObject> points = new();
     public List<GameObject> JumpScarePoints;
     public int AI_Level;
-    private int timer = 0;
-    private int Ai_max = 20; 
-    public bool CanChangePosition = true;
+    public float MoveCheckDistance;
 
-    void Update(){
-if(CanChangePosition == true){
-    if(timer >= 360){
-        if(ShouldSpawn(AI_Level, Ai_max) == true){
-            transform.position = RandomPosition(points).transform.position;
-        }
-        timer -= timer;
+    public float StartTime;
+    private float timer;
 
-
-}
-
-JumpScareCheck();
-timer++;
-}
-}
-
-void JumpScareCheck(){
-    for(int i = 0; i < JumpScarePoints.Count; i++){
-        if(transform.position == JumpScarePoints[i].transform.position){
-            GetComponent<JumpScareScript>().JumpScare();
-    }
-}
-} 
-    GameObject RandomPosition(List<GameObject> points){
-        //picks a random point in its list of walkable points
-        int random = Random.Range(0,8);
-        if(random == 0){
-            
-            GameObject Position = points[Random.Range(0, points.Count)];
-            Debug.Log("I moved");
-            return Position;
-        }else{
-            return this.gameObject;
-        }
+    void Awake()
+    {
+        //sets enemy pos to be its start pos
+        transform.position = points[0].transform.position;
+        timer = StartTime;
     }
 
-    bool ShouldSpawn(int AI_level, int AI_max){
-        //compares if the enemy should walk depending on what difficulty it is on
-        int random3 = Random.Range(0, AI_max);
-        if(random3 < AI_level){
+    void Update()
+    {
+        //checks if the timer for move is 0, then checks if it should move, then moves.
+        if (timer <= 0)
+        {
+            timer = StartTime;
+            if (ShouldMove() == true)
+            {
+
+                transform.position = RandomPosition(points).transform.position;
+
+                foreach (GameObject JSP in JumpScarePoints)
+                {
+                    if (JSP.transform.position == transform.position)
+                    {
+                        GetComponent<JumpScareScript>().JumpScare();
+                    }
+                }
+            }
+        }
+
+        timer -= Time.deltaTime;
+    }
+
+    //randomizes what way the enemy moves 
+    GameObject RandomPosition(List<GameObject> points)
+    {
+        List<GameObject> ValidMoveTargets = new();
+    //we check between all moveable targets is close enough for the enemy to logically move to, if it is, we randomize a new positions from one of those
+
+        foreach(GameObject point in points){
+            if(Vector3.Distance(point.transform.position, transform.position) < MoveCheckDistance){
+                ValidMoveTargets.Add(point);
+            }
+        }
+    int Target = Random.Range(0, ValidMoveTargets.Count);
+        GameObject NewPosition = points[Target];
+        ValidMoveTargets.Clear();   
+        Debug.Log("I moved");
+        return NewPosition;
+    }
+
+
+
+    //randomizes if the enemy should move
+    bool ShouldMove()
+    {
+
+        int RandomOdd = Random.Range(0, points.Count);
+
+        if (AI_Level >= RandomOdd)
+        {
             return true;
         }
         return false;
-
     }
+
 }
